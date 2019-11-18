4587177d836eeb073a7ad0b8b54e05487a353f00
jenishbekor <jenishbek.orozahunov@gmail.com>
2019-11-18 13:45:27 +0600

    version 2

--- src/com/company/Magical.java
+++ src/com/company/Magical.java
@@ -1,23 +1,11 @@
 package com.company;
 
-public class Magical extends Player {
-    private String type;
+public class Magical extends Hero {
 
     public Magical(int health, int hitDamage, String type)
     {
-        super(health,hitDamage);
-        this.type = type;
+        super(health,hitDamage,type);
         super.setName("Magical");
     }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-
 
 }
