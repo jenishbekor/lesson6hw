f8736d2b1f58e072fe3b05cec5b3e7cd4a00abfa
jenishbekor <jenishbek.orozahunov@gmail.com>
2019-11-18 13:48:38 +0600

    version 2

--- src/com/company/Mental.java
+++ src/com/company/Mental.java
@@ -1,21 +1,10 @@
 package com.company;
 
-public class Mental extends Player {
-    private String type;
+public class Mental extends Hero {
 
     public Mental(int health, int hitDamage, String type)
     {
-        super(health,hitDamage);
-        this.type = type;
+        super(health,hitDamage,type);
         super.setName("Mental");
     }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
 }
